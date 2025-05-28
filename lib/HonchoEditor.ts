32cbcf30c2aa5a823f3c91a273b459ea30d63b59
yogis <yogisyo16@gmail.com>
2025-05-28 14:37:43 +0700

    uncomment some of cuntion on lib

--- lib/HonchoEditor.ts
+++ lib/HonchoEditor.ts
@@ -42,16 +42,19 @@ export interface HonchoEditor {
     inputImage: HTMLImageElement,
     canvasRef: HTMLCanvasElement
   ): Promise<void>;
+  
+  // Config History for undo and redo
   configHistotrypush(config: Config): void;
+  
   // // Config setter
   // setShadow(shadow: number): void;
   // setTemp(temp: number): void;
   // setTint(tint: number): void;
 
-  // // Config undo - redo - reset
-  // undo(): Promise<void>;
-  // redo(): Promise<void>;
-  // reset(): void;
+  // Config undo - redo - reset
+  undo(): Promise<void>;
+  redo(): Promise<void>;
+  reset(): void;
 
   // // Config applier
   // applyShadow(shadow: number): void;
