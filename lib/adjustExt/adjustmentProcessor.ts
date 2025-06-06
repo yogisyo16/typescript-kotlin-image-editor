c7a3c4db629ab84458f7b5364dc635c58f41cae3
yogis <yogisyo16@gmail.com>
2025-06-07 01:33:01 +0700

    new commented logic

--- lib/adjustExt/adjustmentProcessor.ts
+++ lib/adjustExt/adjustmentProcessor.ts
@@ -19,7 +19,7 @@ async function applyAllAdjustments(originalImage: cv.Mat, adjustmentPipeline: Ad
 
         const finalImage = new cv.Mat();
         const finalConversionType = originalImage.channels() === 4 ? cv.CV_8UC4 : cv.CV_8UC3;
-        imageToProcess16S.convertTo(finalImage, finalConversionType);
+        imageToProcess16S.convertTo(finalImage, finalConversionType); // Convert back to 8-bit (to make sure is converted)
         imageToProcess16S.delete();
         return finalImage;
 
@@ -37,14 +37,18 @@ async function computeDelta(
 ): Promise<cv.Mat> {
     const cleanup: cv.Mat[] = [];
     try {
-        const image8U_before = new cv.Mat();
-        const conversionType8U = image16S.channels() === 4 ? cv.CV_8UC4 : cv.CV_8UC3;
+        const image8U_before = new cv.Mat(); // 8-bit image
+        const conversionType8U = image16S.channels() === 4 ? cv.CV_8UC4 : cv.CV_8UC3; // converter 8 bit checker
         image16S.convertTo(image8U_before, conversionType8U);
         cleanup.push(image8U_before);
 
+        // Applying the adjustment
         let image8U_after = await adjustmentFunction(image8U_before, value);
         cleanup.push(image8U_after);
 
+        // Converter for image8U_after
+        // and in this where checking for channels for image8U_after
+        // is it 4 or 3 channels after the adjustment
         if (image8U_before.channels() === 4 && image8U_after.channels() === 3) {
             const tempResult = image8U_after;
             image8U_after = new cv.Mat();
@@ -53,6 +57,7 @@ async function computeDelta(
             tempResult.delete();
         }
 
+        // empty mat for later used on subtract of deltaMat logic
         const image16S_before = new cv.Mat();
         const image16S_after = new cv.Mat();
         cleanup.push(image16S_before, image16S_after);
@@ -62,6 +67,9 @@ async function computeDelta(
         image8U_after.convertTo(image16S_after, conversionType16S);
         
         const deltaMat = new cv.Mat();
+
+        // damn it, it should be ez but it's not unfortunately
+        // THIS IS the delta logic for the image
         cv.subtract(image16S_after, image16S_before, deltaMat);
         
         return deltaMat;
